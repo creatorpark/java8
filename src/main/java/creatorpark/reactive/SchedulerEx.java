257ff8ed58e6f52da8effdb7efbd57a3781dd61f
creatorpark <creatorpark@naver.com>
2020-01-10 14:26:11 +0900

    reactive pattern 작업중

---
+++ src/main/java/creatorpark/reactive/SchedulerEx.java
@@ -0,0 +1,24 @@
+package creatorpark.reactive;
+
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscription;
+
+public class SchedulerEx {
+
+	public static void main(String[] args) {
+//		Publisher<Integer> pub = sub -> {
+//			sub.onSubscribe(new Subscription() {
+//				
+//				@Override
+//				public void request(long n) {
+//					sub.onNext(item);
+//				}
+//				
+//				@Override
+//				public void cancel() {
+//					
+//				}
+//			});
+//		});
+	}
+}
